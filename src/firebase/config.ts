141400b12fc6f6ef09fa19bd87a518f8ec7b9d79
Abdul Basit <abdulbasitkhan9934@gmail.com>
2025-10-04 09:32:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5640296892-8b95d",
+  "appId": "1:554219365197:web:8455799cef14bc4e650d04",
+  "apiKey": "AIzaSyB5DgmSsaRSUQvmRdkYWUBmxEAxYwFUlRQ",
+  "authDomain": "studio-5640296892-8b95d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "554219365197"
+};
